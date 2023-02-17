28143cc7b8ba3c7cd0f75c2889cdc0663b8c04d7
PhamIchChuyen220603 <chuyen.pi11316@sinhvien.hoasen.edu.vn>
2023-02-17 09:25:56 +0700

    D07-logInRunWay

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) }, { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) }, { path: 'shared', loadChildren: () => import('./shared/shared.module').then(m => m.SharedModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'fir-1cbfb',
+    appId: '1:283633039556:web:6e2d54bd9ee8245df6b191',
+    storageBucket: 'fir-1cbfb.appspot.com',
+    apiKey: 'AIzaSyADb2Dqe_tJtl7s3TLV_GfiIkDD9VW6DwA',
+    authDomain: 'fir-1cbfb.firebaseapp.com',
+    messagingSenderId: '283633039556',
+  },};
